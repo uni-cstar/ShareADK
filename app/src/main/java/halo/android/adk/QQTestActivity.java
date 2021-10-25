5fc39c3958d469da15d392e7ffcb76920eb8e678
Lucio <super_luo@163.com>
2021-10-25 15:18:44 +0800

    update:去掉bintray发布，改用jitpack

---
+++ app/src/main/java/halo/android/adk/QQTestActivity.java
@@ -0,0 +1,18 @@
+//package halo.android.adk;
+//
+//import android.os.Bundle;
+//
+//import androidx.annotation.Nullable;
+//import androidx.appcompat.app.AppCompatActivity;
+//
+///**
+// * Created by Lucio on 2021/10/25.
+// */
+//public class QQTestActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(@Nullable Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.qq_test_activity);
+//    }
+//}
---
+++ qq/src/test/java/halo/android/integration/qq/ExampleUnitTest.java
@@ -0,0 +1,23 @@
+package halo.android.qq;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void aa() {
+        assertEquals(4, Math.round(4.45) );
+        assertEquals(5, Math.round( 4.5));
+    }
+}
